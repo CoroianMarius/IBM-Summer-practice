18936f89072b815ad747be13496959635b63babf
CoroianMarius <122046063+CoroianMarius@users.noreply.github.com>
2023-07-05 14:55:27 +0300

    Initial commit

---
+++ src/components/molecules/SearchBar.js
@@ -0,0 +1,13 @@
+import Input from '../atoms/Input';
+import Button from '../atoms/Button';
+
+const SearchBar = () => {
+  return (
+    <>
+      <Input />
+      <Button label="Press me" />
+    </>
+  );
+};
+
+export default SearchBar;
---
+++ src/components/organisms/Header.js
@@ -0,0 +1,14 @@
+import SearchBar from '../molecules/SearchBar';
+
+const Header = () => {
+  return (
+    <div>
+      <h2>Logo</h2>
+      <div>
+        <SearchBar />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
