ceadcc7f10296332902a5dc7ef389b3df7800972
Robi <robert.lazarov02@e-uvt.ro>
2023-07-14 20:36:49 +0300

    Navbar component added

---
+++ src/app/groups/page.js
@@ -0,0 +1,11 @@
+import Nav from "@/components/organisms/Nav.js";
+
+export default function Page() {
+    return (
+        <div>
+          {/* Other content of the Groups page */}
+          <Nav />
+          {/* Additional content */}
+        </div>
+      );
+}
---
+++ src/components/organisms/Nav.js
@@ -0,0 +1,25 @@
+"use client";
+
+import Link from "next/link";
+import styles from "../../app/groups/groups.css";
+import image from "../../../public/ibmlogo.png";
+
+
+function Nav() {
+  return (
+    <div className="container">            
+        <img src="/public/next.svg" alt="logo" className="logo" />       
+        <div>
+            <ul id="navbarElements">
+                <li><a href="/groups" className="admin">Admin Panel</a></li>
+                <li><a href="/groups">Groups</a></li>
+                <li><a href="/groups">Calendar</a></li>
+                {/* It will be a btn component */}
+                <li><Link href="/login">Login</Link></li>
+            </ul>
+        </div>
+    </div>
+  );
+}
+
+export default Nav;
