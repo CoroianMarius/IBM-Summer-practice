001048536bd71767617b7bfee67b5d42aa227ab0
CoroianMarius <dan.coroian03@e-uvt.ro>
2023-07-23 18:14:57 +0300

    added the function to get the groups dynamically (buggy version)

--- src/components/organisms/GroupsContainer.js
+++ src/components/organisms/GroupsContainer.js
@@ -4,6 +4,8 @@ import styles from "@/app/groupsV2/groups.module.css"
 import GroupSideBarV2 from "../molecules/GroupSideBarV2"
 import { useGroup } from "@/context/GroupContext"
 import GroupsContent from "../molecules/GroupContent"
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function getGroups(){
     return [
@@ -61,9 +63,27 @@ export default function GroupsContainer(){
 
     const selectedGroup = useGroup()
 
+
+
+
+
+
+
+    const [selectedGroups, setselectedGroups] = useState([]);
+    useEffect(()=>{
+        async function getselectedGroups(){
+            setselectedGroups((await axios.get('http://localhost:5000/groups', {withCredentials:true})).data.groups)
+        }
+        getselectedGroups()
+    }, [])
+    console.log(selectedGroups)
+
+
+
+
     return(
         <div className={styles.groupContainer}>
-            <GroupSideBarV2 groups={getGroups()}/>
+            <GroupSideBarV2 groups={selectedGroups}/>
             {selectedGroup && <GroupsContent group={selectedGroup}/>}
         </div>
     )
