8ce00d7d7a8cb48a73d02022f0a7abdba593d6ce
DaniConstantinescu <65397773+DaniConstantinescu@users.noreply.github.com>
2023-07-23 18:45:40 +0300

    Update SendInvites.js

--- src/components/molecules/SendInvites.js
+++ src/components/molecules/SendInvites.js
@@ -212,7 +212,7 @@ function SendInvites() {
               ))}
             </div>
             
-            <button onClick={handleSubmin}>Adedwa</button>
+            <button onClick={handleSubmin} className={styles.button}>Send Invites</button>
 
           </>
         )}
