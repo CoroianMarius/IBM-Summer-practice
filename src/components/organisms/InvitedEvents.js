09b6ce35c17c55fcd9077387d4c701e0629cd469
Robi <robert.lazarov02@e-uvt.ro>
2023-07-26 00:47:46 +0300

    Resolved InvitedEvents component

--- src/components/organisms/InvitedEvents.js
+++ src/components/organisms/InvitedEvents.js
@@ -4,32 +4,9 @@ import EventCard from "./EventCard"
 import styles from "@/app/events/events.module.css"
 import { useState, useEffect } from "react"
 
-function getInvitedEvents(){
-    return [ 
-        {
-            id: 654651,
-            titlu: "Event 1",
-            data: "12-07-2023 13:00",
-            locatie: "Locatie 1",
-            descriere: "Descriere 1",
-            tags: "Tag 1",
-            participanti: ["User 1", "User 2", "User 3", "User 4", "User 5"],
-        },
-        {
-            id: 151654,
-            titlu: "Event 2",
-            data: "05-11-2023 08:00",
-            locatie: "Locatie 2",
-            descriere: "Descriere 2",
-            tags: "Tag 2",
-            participanti: ["User 1", "User 2", "User 3", "User 4", "User 5"],
-        }
-    ]
-
-}
 
 export default function InvitedEvents() {
-    const [invitedEvents2, setInvitedEvents2] = useState([])
+    const [invitedEvents, setInvitedEvents2] = useState([])
 
     useEffect(() => {
         const getInvitedEvents2 = async () => {
@@ -37,18 +14,17 @@ export default function InvitedEvents() {
             const data = await response.json()
             
             console.log("aici sunt toate eventurile la care sunt invitat'")
-            console.log(data)
+            setInvitedEvents2(data.events);
             }
         getInvitedEvents2()
     }, [])
 
-    const invitedEvents = getInvitedEvents()
 
     return <div className={styles.invitedContainer}>
         <h1 className={styles.header}>Invited</h1>
         <div className={styles.eventsContainer} >
             {invitedEvents?.map((event) => 
-                <EventCard key={event.id} event={event} />
+                <EventCard key={event._id} event={event} />
             )}
         </div>
     </div>
