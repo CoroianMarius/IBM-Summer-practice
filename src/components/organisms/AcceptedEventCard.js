1adac0721fac5121937bb16a4890c9c5edc41c8a
DaniConstantinescu <65397773+DaniConstantinescu@users.noreply.github.com>
2023-07-27 02:18:27 +0300

    Added functionality to Accept/Leave Event

--- src/components/organisms/AcceptedEventCard.js
+++ src/components/organisms/AcceptedEventCard.js
@@ -1,29 +1,49 @@
-import styled from "../../css/eventCard.module.css"
-import EventTag from "../atoms/EventTag"
-import EventDetails from "../molecules/eventDetails"
+import axios from 'axios';
+import React from 'react';
+import styled from "../../css/eventCard.module.css";
+import EventTag from "../atoms/EventTag";
+import EventDetails from "../molecules/eventDetails";
 
-export default function EventCard({event}) {
+export default function EventCard({ event }) {
+  const { _id, title, date, location, description, tags } = event;
 
-    const {_id, title, date, location, description, tags} = event
-    console.log(tags);
+  const leaveEvent = async () => {
+    try {
+      const response = await axios.put(
+        `http://localhost:5000/events/remove/${_id}`,
+        null,
+        { withCredentials: true }
+      );
 
-    return <>
-    <div className={styled.event}>
-        {tags !== "" &&
-            <div className={styled.tags}>            
-                    <EventTag key={_id} tag={tags} />
-            </div>
-        }   
+      const updatedEvent = response.data.event;
+      console.log("You left the event. Updated event:", updatedEvent);
+      window.location.reload();
+
+    } catch (error) {
+      // Handle any errors that might occur during the API call
+      console.error("Error leaving event:", error);
+    }
+  };
+
+  return (
+    <>
+      <div className={styled.event}>
+        {tags !== "" && (
+          <div className={styled.tags}>
+            <EventTag key={_id} tag={tags} />
+          </div>
+        )}
 
         <div className={styled.main}>
-            <EventDetails title={title} date={date} location={location} description={description} />
-            <div className={styled.container}>
-                <button className={styled.leaveBtn}>Leave</button>
-            </div>
+          <EventDetails title={title} date={date} location={location} description={description} />
+          <div className={styled.container}>
+            <button onClick={leaveEvent} className={styled.leaveBtn}>
+              Leave
+            </button>
+          </div>
         </div>
-
-    </div>
-    <br></br>
+      </div>
+      <br />
     </>
-
+  );
 }
--- src/components/organisms/EventCard.js
+++ src/components/organisms/EventCard.js
@@ -1,10 +1,33 @@
 import styles from "../../css/eventCard.module.css"
 import EventTag from "../atoms/EventTag"
 import EventDetails from "../molecules/eventDetails"
+import axios from "axios"
 
 export default function EventCard({event}) {
 
     const {_id, title, date, location, description, tags} = event
+
+    const acceptInvite = async () => {
+        try {
+          const response = await axios.put(
+            `http://localhost:5000/events/upcoming/${_id}`,
+            null, // We don't need to send a request body for this PUT request
+            { withCredentials: true } // Set the withCredentials option to send cookies with the request
+          );
+    
+          // The server should respond with the updated event
+          const updatedEvent = response.data.event;
+          console.log("You accepted the invite. Updated event:", updatedEvent);
+
+          window.location.reload()
+    
+          // You can perform any additional actions with the updated event if needed
+    
+        } catch (error) {
+          // Handle any errors that might occur during the API call
+          console.error("Error accepting invite:", error);
+        }
+      };
 
     return <>
     <div className={styles.event}>
@@ -17,7 +40,7 @@ export default function EventCard({event}) {
         <div className={styles.main}>
             <EventDetails title={title} date={date} location={location} description={description} />
             <div className={styles.container}>
-                <button className={styles.acceptBtn}>Accept</button>
+                <button onClick={acceptInvite} className={styles.acceptBtn}>Accept</button>
             </div>
         </div>
 
